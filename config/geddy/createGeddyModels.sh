8648c3f2487ad0f7dc7967203cd6853a6d75ef80
Ken <ken@kengoldfarb.com>
2013-02-26 22:53:37 -0700

    updates w/ boilerplate

---
+++ config/geddy/createGeddyModels.sh
@@ -0,0 +1,2 @@
+#/bin/sh
+# geddy --swig scaffold commit id:string _id:string githubId:string url:string author:object message:string timestamp:datetime added:array before:string after:string ref:string repository:object
